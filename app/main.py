2895215a51d6d85e9df58ef75617c62a3b1a5879
M. Hanan Brian Prakoso <hanzprakoso27@gmail.com>
2021-09-01 23:13:29 +0700

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+import uvicorn
 from fastapi import Depends, FastAPI
-from .models import db_models
-from .dependencies import get_query_token, get_token_header
-from .routers import product_router
-from .database import SessionLocal, engine
+from app.models import db_models
+from app.dependencies import get_query_token, get_token_header
+from app.routers import product_router
+from app.database import SessionLocal, engine
 
 db_models.metadata.create_all(engine)
 
@@ -10,3 +11,12 @@ app = FastAPI(debug=True, title="Travel System API")
 # app = FastAPI(debug=False, title="Travel System API", docs_url=None, redoc_url=None, openapi_url=None)
 
 app.include_router(product_router.router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",
+                    host="127.0.0.1",
+                    port=7700,
+                    reload=True,
+                    debug=True)
+
+# uvicorn.run(app, host="127.0.0.1", port=8080)

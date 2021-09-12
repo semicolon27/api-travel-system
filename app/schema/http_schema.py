96c107ea12e564757f34e202fc60d78e70557672
Han Prakoso <hanzprakoso27@gmail.com>
2021-09-12 21:06:27 +0700

    set default value for success only

--- app/schema/http_schema.py
+++ app/schema/http_schema.py
@@ -6,8 +6,8 @@ DataT = TypeVar('DataT')
 
 
 class Response(GenericModel, Generic[DataT]):
-    rc: str
-    rcdesc: str
+    rc: str = "000"
+    msg: str = "Success"
     data: Optional[DataT]
     # kalau ngga pake orm_mode, bakalan value is not a valid dict (type=type_error.dict)
     # khusus skema untuk convert dari hasil query ke respon

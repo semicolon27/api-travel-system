f3079b121370452ee5f5d894a23d9274775ea8d2
Han Prakoso <hanzprakoso27@gmail.com>
2021-09-12 21:06:11 +0700

    create city section

--- app/routers/master/city_router.py
+++ app/routers/master/city_router.py
@@ -1,11 +1,14 @@
+"""
+Manajemen data tabel City
+"""
 from app.schema.http_schema import Response
-from app.dependency import get_db
-from typing import List
 from fastapi.params import Depends
 from sqlalchemy.orm import session
+from app.dependency import get_db
 from app.services.master import city_service
-from fastapi import APIRouter
 from app.schema.master.city_schema import City
+from typing import List
+from fastapi import APIRouter
 
 router = APIRouter()
 
@@ -13,28 +16,40 @@ router = APIRouter()
 @router.get("/master/city", response_model=Response[List[City]], tags=["City"])
 async def get_cities(db: session = Depends(get_db)):
     res = Response
-    res.rc = '000'
-    res.msg = 'Sukses'
     res.data = city_service.get_cities(db)
     return res
 
 
-@router.get("/master/city/{city_id}", tags=["City"])
+@router.get("/master/city/{city_id}", response_model=Response[City], tags=["City"])
 async def get_city_by_id(city_id, db: session = Depends(get_db)):
-    categories = city_service.get_city_by_id(db, city_id)
-    return categories
+    res = Response
+    res.data = city_service.get_city_by_id(db, city_id)
+    return res
 
 
-@router.post("/master/city", tags=["City"])
-async def add_city(db: session = Depends(get_db)):
-    return []
+@router.get("/master/city/search/{keyword}", response_model=Response[List[City]], tags=["City"])
+async def search_city(keyword, db: session = Depends(get_db)):
+    res = Response
+    res.data = city_service.search_cities(db, keyword)
+    return res
 
 
-@router.put("/master/city", tags=["City"])
-async def edit_city(db: session = Depends(get_db)):
-    return []
+@router.post("/master/city", response_model=Response[City], tags=["City"])
+async def add_city(city: City, db: session = Depends(get_db)):
+    res = Response
+    res.data = city_service.add_city(db, city)
+    return res
 
 
-@router.delete("/master/city", tags=["City"])
-async def delete_city(db: session = Depends(get_db)):
-    return []
+@router.put("/master/city", response_model=Response[City], tags=["City"])
+async def edit_city(city: City, db: session = Depends(get_db)):
+    res = Response
+    res.data = city_service.edit_city(db, city)
+    return res
+
+
+@router.delete("/master/city", response_model=Response[City], tags=["City"])
+async def delete_city(city: City, db: session = Depends(get_db)):
+    res = Response
+    res.data = city_service.delete_city(db, city)
+    return res
--- app/services/master/city_service.py
+++ app/services/master/city_service.py
@@ -1,15 +1,52 @@
+"""
+Helo
+"""
+from app.schema.master.city_schema import City
 from app.models.db_models import T_City
 from sqlalchemy.orm import Session
 from sqlalchemy import desc
 
 
 def get_cities(db: Session):
-    # db.query ngga bisa pkai await
     all_get = db.query(T_City).order_by(desc(T_City.Name)).all()
     return all_get
 
 
+def search_cities(db: Session, keyword: str):
+    result = db.query(T_City).filter(T_City.Name.like(f"%{keyword}%")).all()
+    print(result)
+    return result
+
+
 def get_city_by_id(db: Session, city_id):
-    # db.query ngga bisa pkai await
-    all_get = db.query(T_City).filter(T_City.CityID == city_id)
-    return all_get
+    city_by_id = db.query(T_City).filter(T_City.CityID == city_id).first()
+    return city_by_id
+
+
+def add_city(db: Session, city: City):
+    db_city = T_City(**city.dict())
+    db.add(db_city)
+    db.commit()
+    db.refresh(db_city)
+    return db_city
+
+
+def edit_city(db: Session, city: City) -> City:
+    edited_city = get_city_by_id(db, city.CityID)
+    city_new = T_City(**city.dict())
+
+    edited_city.Name = city_new.Name
+    edited_city.countryID = city_new.countryID
+    edited_city.locKind = city_new.locKind
+
+    db.commit()
+    db.refresh(edited_city)
+
+    return city
+
+
+def delete_city(db: Session, city: City):
+    deleted_city = get_city_by_id(db, city.CityID)
+    db.delete(deleted_city)
+    db.commit()
+    return deleted_city
